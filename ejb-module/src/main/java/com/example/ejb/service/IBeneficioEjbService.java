1129d80a5cde83fa75a02b6d7ba634808d65568d
Lucas Gabriel <lucasmoreira.gabriel2004@gmail.com>
2026-03-03 23:40:36 -0300

    feat: add IBeneficioEjbService

---
+++ ejb-module/src/main/java/com/example/ejb/service/IBeneficioEjbService.java
@@ -0,0 +1,18 @@
+package com.example.ejb.service;
+
+import com.example.ejb.model.Beneficio;
+import jakarta.ejb.Remote;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Remote
+public interface IBeneficioEjbService {
+    void transfer(Long fromId, Long toId, BigDecimal amount);
+
+    Beneficio criar(String nome, BigDecimal valor);
+
+    void atualizar(Long id, String nome, BigDecimal valor);
+
+    List<Beneficio> buscarTodos();
+}
