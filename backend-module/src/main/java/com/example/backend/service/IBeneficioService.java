77cdd314afbe4763d235e38cc7cb73eac62f4839
Lucas Gabriel <lucasmoreira.gabriel2004@gmail.com>
2026-03-03 23:40:49 -0300

    feat: add IBeneficioService

---
+++ backend-module/src/main/java/com/example/backend/service/IBeneficioService.java
@@ -0,0 +1,14 @@
+package com.example.backend.service;
+
+import com.example.backend.dto.BeneficioRequestDTO;
+import com.example.backend.dto.BeneficioResponseDTO;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface IBeneficioService {
+    public BeneficioResponseDTO criar(BeneficioRequestDTO dto);
+    BeneficioResponseDTO atualizar(Long id, BeneficioRequestDTO dto);
+    List<BeneficioResponseDTO> buscarTodos();
+    void transfer(Long fromId, Long toId, BigDecimal valor);
+}
