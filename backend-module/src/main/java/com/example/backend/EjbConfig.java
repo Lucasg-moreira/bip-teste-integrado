c1f97b6ba3048f4d4299390ef89bdee377c98e3e
Lucas Gabriel <lucasmoreira.gabriel2004@gmail.com>
2026-03-05 10:53:59 -0300

    fix: add ejbconfig props

--- backend-module/src/main/java/com/example/backend/EjbConfig.java
+++ backend-module/src/main/java/com/example/backend/EjbConfig.java
@@ -1,29 +1,34 @@
 package com.example.backend;
 
 import com.example.ejb.service.IBeneficioEjbService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.util.Properties;
 
 @Configuration
 public class EjbConfig {
+    @Value("${ejb.url}")
+    String ejbUrl;
+    @Value("${ejb.context}")
+    String context;
+
     @Bean
-    public IBeneficioEjbService beneficioEjb() throws Exception {
+    public IBeneficioEjbService beneficioEjb() throws NamingException {
 
         Properties props = new Properties();
 
-        props.put(Context.INITIAL_CONTEXT_FACTORY,
-                "org.wildfly.naming.client.WildFlyInitialContextFactory");
+        props.put(Context.INITIAL_CONTEXT_FACTORY, context);
 
-        props.put(Context.PROVIDER_URL,
-                "http-remoting://localhost:8080");
+        props.put(Context.PROVIDER_URL, ejbUrl);
 
-        Context context = new InitialContext(props);
+        Context ctx = new InitialContext(props);
 
-        return (IBeneficioEjbService) context.lookup(
+        return (IBeneficioEjbService) ctx.lookup(
                 "ejb:/ejb-module-1.0.0/BeneficioEjbService!com.example.ejb.service.IBeneficioEjbService"
         );
     }
